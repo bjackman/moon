f12a3d665be76354bc5ad9b7f6630b0e6cf980ee
Brendan Jackman <brendan.jackman@arm.com>
2016-11-20 16:36:19 +0000

    I can read dates now

--- src/moon/core.clj
+++ src/moon/core.clj
@@ -1,7 +1,22 @@
 (ns moon.core
+  (:require [clj-time.core :refer [date-time now]])
+  (:require [clj-time.format])
+  (:require [clojure.java.io :refer [reader]])
+  (:require [clojure.string :refer [split]])
   (:gen-class))
+
+(def date-fmt (clj-time.format/formatters :date))
+
+(defn read-dates
+  "Read some dates from a file"
+  [path]
+  (use 'clojure.java.io)
+  (with-open [rdr (reader path)]
+    (doseq [line (line-seq rdr)]
+      (let [date-str (first (split line #" "))]
+        (println (clj-time.format/parse date-str))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (read-dates "expected_head.txt"))
